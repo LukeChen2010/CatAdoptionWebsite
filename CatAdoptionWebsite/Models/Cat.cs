24ea05046d2342e4aade515fef94d9ddff8fe7e1
Luke Chen <luke.chen.2010@gmail.com>
2020-09-28 21:45:55 -0500

    Initial repo create

---
+++ CatAdoptionWebsite/Models/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatAdoptionWebsite.Models
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Breed { get; set; }
+        public bool SpayedNeutered { get; set; }
+        public bool IsAdopted { get; set; }
+    }
+}
